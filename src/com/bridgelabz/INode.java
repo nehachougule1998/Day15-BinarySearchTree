e3ef1cdd4a74ccef105a8e5a286582d3551ca3e8
nehachougule1998 <nchougule734@gmail.com>
2022-03-27 16:53:04 +0530

    UC1 added

--- src/com/bridgelabz/INode.java
+++ src/com/bridgelabz/INode.java
@@ -1,9 +1,13 @@
 package com.bridgelabz;
 
-public class INode {
-	
-	    int data;
-	    INode left;
-	    INode right;
-
-	}
+public class INode<K extends Comparable<K>>  {
+	 K key;
+	 INode<K> left;
+	 INode<K> right;
+	 
+	 public INode(K key){
+		 this.key = key;
+	     this.left= null;
+	     this.right= null;
+	 }
+}
